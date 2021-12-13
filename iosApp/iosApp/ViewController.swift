7ba10a8597bd0aad3ad6863cff77e423b088f01c
Raul Guzman <rguzmanc.161@gmail.com>
2021-12-12 23:38:45 -0500

    Feature: Update from SwiftUI to Storyboards

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by Raul Jonathan Guzman Condor on 12/12/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
