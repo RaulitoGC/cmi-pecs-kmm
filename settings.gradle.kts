0b669fff34a0be78757f8d2469e5a218dd95e5fd
Raul Guzman <rguzmanc.161@gmail.com>
2021-12-12 22:22:13 -0500

    Feature: First project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "cmipecs"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/rguzmanc/cmipecs/Platform.kt
@@ -0,0 +1,5 @@
+package com.rguzmanc.cmipecs
+
+expect class Platform() {
+    val platform: String
+}
